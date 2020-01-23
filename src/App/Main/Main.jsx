d7bb24c80da9854c9219ff498720fc6f08dea6e2
Drew Winkler <drewwinkler4@gmail.com>
2020-01-22 21:45:13 -0800

    change

--- src/App/Main/Main.jsx
+++ src/App/Main/Main.jsx
@@ -5,7 +5,7 @@ const Main = () => {
     return (
         <main className='Main'>
             <div className="columns">
-                <div className="col col1">Col1</div>
+                <div className="col col1">Col1 change</div>
                 <div className="col col2">Col2</div>
                 <div className="col col3">Col3</div>
                 <div className="col col4">Col4</div>
