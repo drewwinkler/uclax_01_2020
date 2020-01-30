b82ab3e553c1b0f05db40b9eaf2d626a6956bded
Drew Winkler <drewwinkler4@gmail.com>
2020-01-29 20:56:20 -0800

    Added active tabs and tabbed content for the end of class session #7

--- src/App/Pages/Welcome.jsx
+++ src/App/Pages/Welcome.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Pages.scss';
+import Tabbed from '../Tabbed/Tabbed.jsx'
 
 const Welcome = () => {
     return (
         <div className={ 'Pages Welcome' }>
             Welcome
+            <Tabbed />
 
             <div className="columns">
                 <div className="col col1">Col1 change</div>
