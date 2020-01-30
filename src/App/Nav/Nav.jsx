58a3508046a37a677dd65b939ebdd69146f5ebec
Drew Winkler <drewwinkler4@gmail.com>
2020-01-29 19:23:21 -0800

    Refactored Navigation Hamburger Toggle Menu

--- src/App/Nav/Nav.jsx
+++ src/App/Nav/Nav.jsx
@@ -6,66 +6,45 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Nav = () => {
 
-//STATE
-    const[ showMenu, updateShowMenu ] = useState(true);
-    const[ isSmall, updateIsSmall ] = useState(true);
+    const [showMenu, updateShowMenu] = useState(false);
 
-    //FUNCTIONS / Methods
-    //Hamburger Click
-    const handlehamburgerclick =() => {
-        console.log('You hit the hamburger');
-        
-        let timbuktu;
-        
-        if (showMenu === true) {
-                timbuktu = false;
+    // Click Event
+    const handleHamburgerClick = () => {
+        if (showMenu === false) {
+            updateShowMenu(true);
         } else {
-            timbuktu = true;
+            updateShowMenu(false);
         }
-
-        updateShowMenu(timbuktu);
     }
 
-    //Checking for Small view or medium/Large
-    const handleIsSmall = () => {
-        
-        console.log('isSmall', isSmall);
-
-        let newValue;
-
-            if (window.innerWidth > 599) {
-                updateIsSmall (false);
-                updateShowMenu(true);
-            } else {
-                updateIsSmall (true);
-                updateShowMenu(false);
-            }
+    //Window Resize Event
+    const handlewindowresize = () => {
+        if (window.innerWidth > 599) {
+            updateShowMenu(true);
+        } else {
+            updateShowMenu(false);
+        }
     }
 
-
-    //useEffect says only do this when our Nav mounts for the firt time
+    //Only when componet first mounts
     useEffect(()=>{
-    window.addEventListener('resize', handleIsSmall);
-    handleIsSmall();//fire off when page first loads.
-    }, []);
+        window.addEventListener('resize', handlewindowresize);
+    },[]);
 
     return (
         <nav className='Nav'>
-            {
-                isSmall &&
-            <div className="hamburger" onClick={ handlehamburgerclick } >
+            
+            <div className="hamburger" on onClick={ handleHamburgerClick }>
                 <FontAwesomeIcon icon={ faBars } />
                 </div>
-        }
-        {
-            showMenu &&
-        <div className="links">
-            <a href="#">Welcome</a>
-            <a href="#">Services</a>
-            <a href="#">Contact</a>
-            </div>
-        } 
-
+                {
+                showMenu&&
+                <div className="links">
+                <a href="#">Welcome</a>
+                <a href="#">Services</a>
+                <a href="#">Contact</a>
+                </div>
+                }
         </nav>
     )
 };
