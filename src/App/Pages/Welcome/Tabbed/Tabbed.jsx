36ed4c6a3aa4c0cb876041b84de0bc452201bef6
Drew Winkler <drewwinkler4@gmail.com>
2020-02-03 21:08:56 -0800

    Added Services and Staff looping, this weird thing mitch showed us

--- src/App/Pages/Welcome/Tabbed/Tabbed.jsx
+++ src/App/Pages/Welcome/Tabbed/Tabbed.jsx
@@ -21,7 +21,7 @@ const tabItems = [
 
 const Tabbed = () => {
 
-    const [currentTab, setCurrentTab] = useState( tabItems[0]);
+    const [currentTab, setCurrentTab] = useState(tabItems[0]);
 
     const isAbout = (currentTab.title === 'About') ? 'tab active': 'tab';
     const isMission = (currentTab.title === 'Mission') ? 'tab active': 'tab';
@@ -31,13 +31,13 @@ const Tabbed = () => {
         <div className={ 'Tabbed' }>
 
             <div className="tabs">
-                <div onClick={ () => { setCurrentTab(tabItems [0]) } } className={ isAbout }>About</div>
-                <div onClick={ () => { setCurrentTab(tabItems [1]) } } className={ isMission }>Mission</div>
-                <div onClick={ () => { setCurrentTab(tabItems [2]) } } className={ isVision }>Vision</div>
+                <div onClick={ () => { setCurrentTab(tabItems[0]) } } className={ isAbout }>About</div>
+                <div onClick={ () => { setCurrentTab(tabItems[1]) } } className={ isMission }>Mission</div>
+                <div onClick={ () => { setCurrentTab(tabItems[2]) } } className={ isVision }>Vision</div>
             </div>
 
             <div className="content">
-                <img src={ currentTab.image }/>
+                <img src={ currentTab.image } alt={ currentTab.title }/>
                 <h2>{ currentTab.title }</h2>
                 <p>{ currentTab.text }</p>
             </div>
